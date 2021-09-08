086787443ffea73b648cacd639a20c5ecaef2222
LearnRikai <levii.reys@gmail.com>
2021-09-08 21:08:42 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,15 +4,16 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
     public function run()
     {
-        DB::insert('insert into roles (id, role_name) values (?, ?)', [1, 'ADMIN']);
-        DB::insert('insert into roles (id, role_name) values (?, ?)', [2, 'USER']);
-        $this->call([
-            UserSeeder::class,
+        DB::table('users')->insert([
+            'name' => 'ADMIN',
+            'email' => 'admin@gmail.com',
+            'password' => Hash::make('123456789'),
         ]);
     }
     
